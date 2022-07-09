f84e60f1e9319388f4b48e693ab23b670553e47c
Muhammed Polat <mpolat21@ku.edu.tr>
2022-07-09 11:21:10 +0300

    the first function served as static

--- index.js
+++ index.js
@@ -7,16 +7,16 @@
 'use strict';
 
 /**
- * Expose `arrayFlatten`.
+ * Expose `Prompter`.
  */
- module.exports = {
-    promptQuestion:promptQuestion,
- }
+ module.exports = Prompter;
+
+ class Prompter{
 
  /**
  * Prompts the question to the console
  * Returns a promise that will resolve when the user inputs and hits the enter button.
- * writin exit kills the terminal
+ * Writing "exit" to the console kills the terminal
  * 
  * 
  * @param  {String} question
@@ -24,7 +24,7 @@
  * 
  */
 
- function promptQuestion(question){
+ static promptQuestion(question){
     const questionPromise = new Promise((resolve, reject) => {
         process.stdout.write(question+'\n');
         
@@ -44,3 +44,4 @@
     });
     return questionPromise;
  }
+}
