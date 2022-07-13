c74a61eefaed7dc21336e4fcfa3443716058507a
Muhammed Polat <mpolat21@ku.edu.tr>
2022-07-13 12:05:38 +0300

    Version 1.0.0

--- index.js
+++ index.js
@@ -7,12 +7,27 @@
 'use strict';
 
 
+/*!
+ * This class provides all the functionality
+ * All the methods in this class are static 
+ * So you do not need to instantiate it
+ */
+
 
  class Dialog{
 
 
+
+
+/*!
+ * colors and styles objects are initialized
+ */
+
 static colors = require('./lib/colors');
 static styles = require('./lib/styles');
+
+
+
 
  /**
  * Prompts the question to the console
@@ -21,18 +36,26 @@ static styles = require('./lib/styles');
  * 
  * 
  * @param  {String} question
+ * @param  {./lib/colors|.lib/style} style
+ * 
  * @return {Promise}
  * 
  */
 
- static promptQuestionPromise(question){
+ static promptQuestionPromise(question,style){
     const questionPromise = new Promise((resolve, reject) => {
-        process.stdout.write(`\x1b[93m${question} : \x1b[39m`);
+        
+        if (style) this.#setTerminalColor(style);
+        process.stdout.write(question+" : ");
+
+        this.#setTerminalColor(this.colors.yellow);
         
         process.stdin.once('data', userInput =>{
             if(userInput =='exit\r\n') process.exit();
         
-            const cleanedInput = userInput.toString().replace(/(\r\n|\n|\r)/gm,"");
+
+            this.#resetTerminalStyle();
+            const cleanedInput = this.#sanitizeUserInput(userInput);
             resolve(cleanedInput);
             
         
@@ -46,6 +69,54 @@ static styles = require('./lib/styles');
     return questionPromise;
  }
 
+  /**
+ * Prompts the question to the console
+ * Calls the callback function with the argument written by the user
+ * Writing "exit" to the console kills the terminal
+ * 
+ * 
+ * @param  {String} question
+ * @param  {Function} callback
+ * @param  {./lib/colors|.lib/style} style
+ * 
+ * @return {Function(userInput)} callback
+ * 
+ */
+
+   static promptQuestionCallback(question,callback,style){
+   
+        const message = (style) ? `\x1b[${style[0]}m ${question} : \x1b[${style[1]}m ` : `${question} : `;
+        process.stdout.write(message);
+        
+        this.#setTerminalColor(Dialog.colors.bgRed);
+
+        process.stdin.once('data', userInput =>{
+            if(userInput =='exit\r\n') process.exit();
+        
+            this.#resetTerminalStyle();
+            const cleanedInput = this.#sanitizeUserInput(userInput);
+            callback(cleanedInput);
+            });
+
+ }
+
+  /**
+ *Write the text down to the terminal
+ * 
+ * 
+ * @param  {String} text
+ * @param  {./lib/colors|.lib/style} style
+ */
+
+ static promptText(text , style){
+    const message = (style) ? `\x1b[${style[0]}m${text} \x1b[${style[1]}m` : text;
+    process.stdout.write(message);
+
+    this.#resetTerminalStyle();
+    this.#newLine();
+
+ }
+
  /**
  * Kill the terminal
  */
@@ -55,6 +126,55 @@ static styles = require('./lib/styles');
         process.exit(0); // kills the terminal
 
  }
+
+
+/**
+ * reset the terminal style
+ */ 
+
+    static #resetTerminalStyle(){
+
+    process.stdout.write(`\x1b[0}\x1b[0m`);
+    }
+
+/**
+ * line break
+ */ 
+
+    static #newLine(){
+
+        // n\ line 
+   process.stdout.write(`\n`);
+   }
+
+/**
+ * @param  {./lib/colors|.lib/style} style
+ */ 
+
+    static #setTerminalColor(style){
+
+        // n\ line 
+        process.stdout.write(`\x1b[${style[0]}m\x1b[${style[1]}m`);
+   }
+
+
+
+/**
+ * Terminal provides the user input with some formatting at the the end of it
+ * 
+ * @param  {String} userInput
+ * 
+ * @return {String}
+ */ 
+
+ static #sanitizeUserInput(userInput){
+
+    
+    return userInput.toString().replace(/(\r\n|\n|\r)/gm,"");;
+  
+  }
+
+
 }
 
 /**
